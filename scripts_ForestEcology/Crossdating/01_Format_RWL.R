325c4d9918b18e75f748c831b271b51f5fd7cd93
Christy Rollinson <crollinson@gmail.com>
2019-11-13 09:52:12 -0600

    File path updates for combining RWL files

--- scripts_ForestEcology/Crossdating/01_Format_RWL.R
+++ scripts_ForestEcology/Crossdating/01_Format_RWL.R
@@ -18,16 +18,16 @@
 # fplot <- dir(path.raw, ".rwl")
 
 # Soft-coding approach = better more flexible code, but easier to break if you don't understand what's going on
-PLOT <- "PLOC-W" #
-path.dat <- "/Volumes/GoogleDrive/My Drive/Forestry Plots (1)/Rollinson_2019_REU_ForestryPlots/data/"
-fplot <- dir(file.path(path.dat, "RingsWidths_Raw/preliminary", PLOT), ".rwl")
+PLOT <- "QUBI-W" #
+path.dat <- "/Volumes/GoogleDrive/My Drive/Forestry Plots/Rollinson_2019_REU_ForestryPlots/data/RingsWidths_Raw/fall 2019"
+fplot <- dir(file.path(path.dat, PLOT), ".rwl")
 
 for(i in 1:length(fplot)){
   # need to read in a file and rename it; i=1
   nm.new <- strsplit(fplot[i], "-")[[1]]
   nm.new <- paste0(nm.new[3:5], collapse="")
   
-  fnow <- dplR::read.tucson(file.path(path.dat, "RingsWidths_Raw/preliminary", PLOT, fplot[i]))
+  fnow <- dplR::read.tucson(file.path(path.dat, PLOT, fplot[i]))
   names(fnow) <- nm.new
   
   if(i==1){
@@ -37,11 +37,12 @@ for(i in 1:length(fplot)){
   }
 }
 summary(rwl.all)
-
+class(rwl.all)
+summary(data.frame(rwl.all))
 
 # Where you want to save the file
-path.out <- file.path("COFECHA/", PLOT)
-if(!dir.exists(path.out)) dir.create(path.out)
+path.out <- file.path(path.dat, "Combined", PLOT)
+if(!dir.exists(path.out)) dir.create(path.out, recursive=T)
 # What you want the name of the file to be
 # file.prefix <- "TEST"
 file.prefix <- paste(PLOT, Sys.Date(), sep="_")
@@ -54,6 +55,8 @@ while(file.exists(file.path(path.out, paste0(file.prefix, ".rwl")))){
 
 dplR::write.rwl(rwl.all, file.path(path.out, paste0(file.prefix, ".rwl")), long.names=T)
 
+# Check Crossdating
 xdate1 <- dplR::corr.rwl.seg(rwl.all, seg.length=10)
-
+summary(xdate1)
+xdate1$flags
 # -------------
